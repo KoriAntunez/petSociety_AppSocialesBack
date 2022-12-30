026155a2e7d31842700304e68e3a5b47ed7dff7e
KoriAntunez <kori.antunez@unmsm.edu.pe>
2022-12-30 14:19:29 -0500

    commit

--- src/models/service.js
+++ src/models/service.js
@@ -1,35 +1,38 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const { Schema } = mongoose;
 
-const ServiceSchema = new Schema({
-    title: { 
-        type: String, 
-        required: true 
+const ServiceSchema = new Schema(
+  {
+    title: {
+      type: String,
+      required: true,
     },
     description: {
-        type: String, 
-        required: true
+      type: String,
+      required: true,
     },
     image: {
-        type: String
+      type: String,
     },
     price: {
-        type: Number,
-        required: true
+      type: Number,
+      required: false,
     },
     quantity_max: {
-        type: Number,
-        required: true
+      type: Number,
+      required: false,
     },
     tutor_id: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "Tutor",
-        required: true
-    }
-}, {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Tutor",
+      required: true,
+    },
+  },
+  {
     timestamps: true,
-    versionKey: false
-});
+    versionKey: false,
+  }
+);
 
-module.exports = mongoose.model('Service', ServiceSchema);
+module.exports = mongoose.model("Service", ServiceSchema);
